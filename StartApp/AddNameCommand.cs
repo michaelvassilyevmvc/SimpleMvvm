0fc292418ab6a20bbead994d98fbed9926a7aa55
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-01-17 14:55:03 +0600

    Add project files.

---
+++ StartApp/AddNameCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace StartApp
+{
+    public class AddNameCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        Model parent;
+
+        public AddNameCommand(Model parent)
+        {
+            this.parent = parent;
+            parent.PropertyChanged += delegate { CanExecuteChanged?.Invoke(this, EventArgs.Empty); };
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return !string.IsNullOrEmpty(parent.CurrentName);
+        }
+
+        public void Execute(object? parameter)
+        {
+            parent.AddedNames.Add(parent.CurrentName);
+            parent.CurrentName = null;
+        }
+    }
+}
---
+++ StartApp/Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace StartApp
+{
+    public class Model: INotifyPropertyChanged
+    {
+        string mCurrentName;
+        public string CurrentName {
+            get
+            {
+                return mCurrentName;
+            }
+            set
+            {
+                if (value == mCurrentName) return;
+                mCurrentName = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public ObservableCollection<string> AddedNames { get; } = new ObservableCollection<string>();
+
+        public ICommand AddCommand { get; private set; }
+
+        public Model()
+        {
+            AddCommand = new AddNameCommand(this);
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
